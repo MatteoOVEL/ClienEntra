7e339d51d2a9d4656970b7f0823ce5b9da4c25b4
ovelm <ovelm@D101-S29>
2024-02-19 17:23:18 +0100

    Ajoutez des fichiers projet.

---
+++ ClienEntra/Models/Chanteur.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienEntra.Models
+{
+    public class Chanteur
+    {
+        public int Idchanteur { get; set; }
+        public string Nomchanteur { get; set; }
+    }
+}
---
+++ ClienEntra/Models/Musique.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienEntra.Models
+{
+    public class Musique
+    {
+        public int Idmusique { get; set; }
+
+        public string Titre { get; set; }
+
+        public string Genre { get; set; }
+        public Chanteur Chanteur { get; set; }
+
+
+    }
+}
